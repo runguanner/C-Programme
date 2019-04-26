622d0f7cd16fdbe9433b37d4044182802273b36d
runguanner <33453382+runguanner@users.noreply.github.com>
2019-04-26 21:25:34 +0800

    考点：数组

---
+++ 119. 杨辉三角 II（Easy）.cpp
@@ -0,0 +1,20 @@
+// 数组
+
+
+// 题目额外限制条件：程序只能使用O(k)的额外空间，那么这样就不能把每行都算出来。
+
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> res(rowIndex + 1);
+        res[0] = 1;
+        for(int i = 1; i <= rowIndex; i++) {
+            for(int j = i; j >= 1; j--) {
+                res[j] += res[j - 1];
+            }
+        }
+        return res;
+    }
+};
+
+
