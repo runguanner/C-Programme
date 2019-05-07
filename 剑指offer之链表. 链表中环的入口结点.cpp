7382ea7966a448c3cb6bb3dbceb6f7cce7d35951
runguanner <33453382+runguanner@users.noreply.github.com>
2019-05-07 15:14:16 +0800

    考点：链表

--- 剑指offer之链表. 链表中环的入口结点.cpp
+++ 剑指offer之链表. 链表中环的入口结点.cpp
@@ -31,3 +31,28 @@ public:
         return nullptr;
     }
 };
+
+
+// 延申出一个小知识点：判断链表有环
+
+class Solution
+{
+public:
+    bool hasCycle(ListNode *head)
+    {
+        if (head == nullptr || head->next == nullptr) return false;
+        ListNode* fast,*slow;
+        slow = fast = head;
+       
+        while(fast && fast->next) //如果有环，fast不可能为nullptr，无环时，fast会运行到末尾null,退出循环
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast) 
+                return true;          
+        }
+        return false;     
+    }
+};
+    
+
