fea8e2d3d6e94ce6985832e0a8c1be78fab9369d
runguanner <33453382+runguanner@users.noreply.github.com>
2019-05-24 16:02:01 +0800

    考点：最小堆、partition函数

---
+++ 剑指offer/查找与排序. 数组中第K大的数.cpp
@@ -0,0 +1,48 @@
+/最小堆、partition函数
+
+
+//方法一：最小堆
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {  //最小堆，最后留下来的就是最大的K个数，堆顶就是第K大的数
+        priority_queue<int, vector<int>, greater<int> > Q;
+        for(int i = 0; i < nums.size(); i++) {  //遍历nums数组
+            if(Q.size() < k) {  //如果堆顶元素小于K个，则直接push元素进入堆中
+                Q.push(nums[i]);
+            }
+            else if(Q.top() < nums[i]) {  //如果堆顶比新元素小，则弹出堆顶元素，push入新元素（即替换堆顶）
+                Q.pop();
+                Q.push(nums[i]);
+            }
+        }
+        return Q.top();  //返回堆顶。即为第K大的数
+    }
+};
+
+//方法二：partition函数
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int left = 0, right = nums.size() - 1;
+        while (true) {
+            int pos = partition(nums, left, right);
+            if (pos == k - 1) return nums[pos];
+            else if (pos > k - 1) right = pos - 1;
+            else left = pos + 1;
+        }
+    }
+    int partition(vector<int>& nums, int left, int right) {
+        int pivot = nums[left], l = left + 1, r = right;
+        while (l <= r) {
+            if (nums[l] < pivot && nums[r] > pivot) {
+                swap(nums[l++], nums[r--]);
+            }
+            if (nums[l] >= pivot) ++l;
+            if (nums[r] <= pivot) --r;
+        }
+        swap(nums[left], nums[r]);
+        return r;
+    }
+};
+
+
