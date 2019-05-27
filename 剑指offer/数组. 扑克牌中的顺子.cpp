bb874d297f3b5a726480d1a71fc774230cc2eff4
runguanner <33453382+runguanner@users.noreply.github.com>
2019-05-27 17:05:56 +0800

    考点：数组

---
+++ 剑指offer/数组. 扑克牌中的顺子.cpp
@@ -0,0 +1,31 @@
+// 数组
+
+
+
+class Solution {
+public:
+    bool IsContinuous( vector<int> numbers ) {
+        if(numbers.empty()) return false;
+        int num_of_zero = 0;
+        int num_of_interval = 0;
+        
+        sort(numbers.begin(), numbers.end());
+        for(int i = 0; i < numbers.size() - 1; i++) {//注意这里是0~n-2的范围，因为后面需要计算numbers[i+1]
+            //统计癞子数量
+            if(numbers[i] == 0) {
+                num_of_zero++;
+                continue;//继续循环
+            }
+            //如果存在对子，直接返回
+            if(numbers[i] == numbers[i + 1]) 
+                return false;
+            //统计间隔数量
+            num_of_interval += numbers[i + 1] - numbers[i] - 1;
+        }
+        if(num_of_zero >= num_of_interval) return true;
+        else return false;
+    }
+};
+
+
+
